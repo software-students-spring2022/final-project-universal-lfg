9d823173524793e8ca4941f08490532cee07676d
Theo <tstephen@tcd.ie>
2022-03-16 22:39:34 -0400

    Added navigation menu + theme.js, containing the colour theme

--- front-end/App.js
+++ front-end/App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Components/Home'
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import Theme from './theme'
 
 export default function App() {
+  const Drawer = createDrawerNavigator(); 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer theme={Theme}> 
+        <Drawer.Navigator screenOptions={{drawerType:'front', headerTitle:'ULFG', headerTintColor: Theme.colors.primary}}>
+          <Drawer.Screen name='Home' component={Home}/>
+          <Drawer.Screen name='Settings' component={Home}/>
+          <Drawer.Screen name='Messages' component={Home}/>
+          <Drawer.Screen name='Posts' component={Home}/>
+        </Drawer.Navigator> 
+      </NavigationContainer>
+   </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
