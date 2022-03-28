52e157653994a664f82f7c738cbb77221294f0c6
patorocks15 <pd2159@nyu.edu>
2022-03-28 14:27:48 -0400

    Added a profile page
    This is the profile page (doesnt have buttons yet)

---
+++ front-end/Screens/Profile.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import { StyleSheet, Text, View, Dimensions, Image  } from 'react-native';
+import { ThemeColors } from 'react-navigation';
+import theme from "../theme";
+import GameCard from '../Components/GameCard';
+const windowWidth = Dimensions.get('window').width;
+ 
+export default function Profile(props){ 
+
+    let emailFromDatabase = 'placeholder';
+    let passwordFromDatabase = '******** (placeholder)';
+    let age = 'placeholder';
+    let gender = 'placeholder';
+    let icon = <View style={styles.emptyCard}><Text style={styles.emptyText}> <Image source={require('front-end/Images/AddIcon.png')} /></Text></View>;
+    return ( 
+        
+        <View style={styles.container}>
+            <Text style={styles.profileTop}>Profile</Text>
+            <Text style={styles.icon}> {icon}</Text>
+            <Text style={styles.email}>Email: {emailFromDatabase} </Text>
+            <Text style={styles.password}>Password: {passwordFromDatabase}</Text>
+            <Text style={styles.age}>Age (optional): {age}</Text>
+            <Text style={styles.gender}>Gender (optional): {gender} </Text>
+           
+        </View>
+  );
+
+
+     
+}
+
+const styles = StyleSheet.create({ 
+    container: { 
+        backgroundColor: theme.colors.background, 
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingTop: 0
+    },
+    
+    text: { 
+        color: theme.text
+    },
+
+    profileTop : {
+        color: 'white',
+        fontSize: 35,
+        fontWeight: "bold",
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+    email : {
+        color: Theme.colors.primary,
+        fontSize: 15,
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+    password : {
+        color: Theme.colors.primary,
+        fontSize: 15,
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+    age : {
+        color: Theme.colors.primary,
+        fontSize: 15,
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+    icon : {
+        color: Theme.colors.primary,
+        fontSize: 15,
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+    gender : {
+        color: Theme.colors.primary,
+        fontSize: 15,
+        paddingBottom: 13,
+        paddingLeft: 13
+    },
+
+
+    emptyCard: { 
+        height: windowWidth*0.5, 
+        width: windowWidth*0.5,
+        borderStyle: 'solid',
+        borderWidth: 3,
+        borderColor: theme.colors.primary,
+        alignItems: 'center',
+        justifyContent: 'center'
+      }
+
+
+    
+})
