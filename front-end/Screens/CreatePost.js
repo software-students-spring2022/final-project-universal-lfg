b24acc35aaf93d1d8ac258e9298eeba0e413ac8b
ash3n <33986672+ash3n@users.noreply.github.com>
2022-04-07 17:52:01 -0400

    start createPost

---
+++ front-end/Screens/CreatePost.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,TextInput } from 'react-native';
+import { Avatar, Button, BottomSheet, Icon, ListItem } from 'react-native-elements'
+import { ScrollView } from 'react-native-gesture-handler';
+import ProgressBar from '../Components/ProgressBar';
+import theme from '../theme';
+import BackButton from '../Components/BackButton';
+import AppButton from '../Components/AppButton'
+import { NavigationEvents } from 'react-navigation';
+
