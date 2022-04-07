cef62d86b427c9f78138dc2bba1cc611f7473bb6
ash3n <33986672+ash3n@users.noreply.github.com>
2022-04-07 17:52:21 -0400

    createPost React states

--- front-end/Screens/CreatePost.js
+++ front-end/Screens/CreatePost.js
@@ -8,3 +8,23 @@ import BackButton from '../Components/BackButton';
 import AppButton from '../Components/AppButton'
 import { NavigationEvents } from 'react-navigation';
 
+export default function ViewPost({route, navigation}){
+    const { game, name, initial, image, rank, detail } = route.params
+
+    const [title, setTitle] = useState({ value: '', error: '' })
+    const [bodyText, setBodyText] = useState({ value: '', error: '' })
+    const [gameMode, setGameMode] = useState({ value: '', error: '' })
+    const [numPlayers, setNumPlayers] = useState({ value: '', error: '' })
+    const [preferredRank, setPreferredRank] = useState({ value: '', error: '' })
+
+    const [activeSpot, setActiveSpot] = useState(1);
+    const totalSpots = 5;
+    const onSubmitPressed = () => {
+        if (activeSpot <= totalSpots) {
+            console.log("One Player Joined the Team");
+            setActiveSpot(prev => prev + 1);
+        } else {
+            console.log("The Team is Full")
+        }
+    }
+}
