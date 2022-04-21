2dcff096eec50d09fc6a4dadda8f4e695b6a3f5a
Helen <helenyxdong@gmail.com>
2022-04-21 17:57:39 -0400

    Minor import error fix in frontend

--- front-end/Screens/Messages.js
+++ front-end/Screens/Messages.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View  } from 'react-native';
 import theme from "../theme";
 import MessageCard from '../Components/MessageCard';
